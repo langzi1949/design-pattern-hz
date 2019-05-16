166b7fa0d63c3e5fdddc9c97d0097b95f016f6b8
chenzehao <chenzehao258@hfjy.com>
2019-05-16 19:49:54 +0800

    add singleton

--- singleton/src/main/java/com/zmglove/SimpleSingleton.java
+++ singleton/src/main/java/com/zmglove/SimpleSingleton.java
@@ -1,11 +1,21 @@
 package com.zmglove;
 
 /**
- * TODO
+ * 简单的单例类
  *
  * @author CZH
  * @version 1.0
  * @date 2019/5/16 19:34
  **/
 public class SimpleSingleton {
+    private static final SimpleSingleton INSTANCE = new SimpleSingleton();
+
+    /**
+     * 私有的构造器 不允许 进行直接的new对象
+     */
+    private SimpleSingleton(){}
+
+    public static SimpleSingleton getInstance(){
+        return INSTANCE;
+    }
 }
