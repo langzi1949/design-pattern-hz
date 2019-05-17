572cee2927a8bfa93aa501557e3d44ef372c9e4b
chenzehao <chenzehao258@hfjy.com>
2019-05-17 18:34:20 +0800

    simple factory pattern

---
+++ simple-factory/src/main/java/com/zmglove/Basketball.java
@@ -0,0 +1,11 @@
+package com.zmglove;
+
+/**
+ * TODO
+ *
+ * @author CZH
+ * @version 1.0
+ * @date 2019/5/17 17:08
+ **/
+public class Basketball {
+}
--- singleton/README.md
+++ singleton/README.md
@@ -1,4 +1,4 @@
 #### 单例模式
 
 少说废话，直接上图:
-![]('etc/singleton-pattern.png')
+![](etc/singleton-pattern.png)
