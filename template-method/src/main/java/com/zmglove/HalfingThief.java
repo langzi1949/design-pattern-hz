2de723e93e92d81c94226a20ec2294e14f81f58b
langzi <chenzehao_1949@126.com>
2019-05-25 22:27:35 +0800

    template pattern add test case

---
+++ template-method/src/main/java/com/zmglove/HalfingThief.java
@@ -0,0 +1,21 @@
+package com.zmglove;
+
+/**
+ *
+ */
+public class HalfingThief {
+
+    private StealingMethod method;
+
+    public HalfingThief(StealingMethod method) {
+        this.method = method;
+    }
+
+    public void steal() {
+        method.steal();
+    }
+
+    public void changeMethod(StealingMethod method) {
+        this.method = method;
+    }
+}
--- template-method/src/test/java/com/zmglove/HitAndRunMethodTest.java
+++ template-method/src/test/java/com/zmglove/HitAndRunMethodTest.java
@@ -1,7 +1,8 @@
 package com.zmglove;
 
 import org.junit.Test;
-import static org.mockito.Mockito.mock;
+
+import static org.mockito.Mockito.*;
 
 /**
  * 测试类
@@ -11,11 +12,12 @@ public class HitAndRunMethodTest {
     @Test
     public void testSteal(){
         final StealingMethod stealingMethod = mock(StealingMethod.class);
-        
+        final HalfingThief thief =  new HalfingThief(stealingMethod);
 
+        thief.steal();
+        verify(stealingMethod).steal();
 
-
-
+        verifyNoMoreInteractions(stealingMethod);
 
     }
 }
