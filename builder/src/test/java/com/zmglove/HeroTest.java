541b60d4ebe017ac3b2e0ce82aaa141a87dbb3ed
chenzehao <chenzehao258@hfjy.com>
2019-05-30 15:55:10 +0800

    :smiley: modify test class

--- builder/src/test/java/com/zmglove/HeroTest.java
+++ builder/src/test/java/com/zmglove/HeroTest.java
@@ -1,6 +1,6 @@
 package com.zmglove;
 
-import com.zmglove.other.Hero;
+import com.zmglove.other.*;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,5 +15,30 @@ public class HeroTest {
     @Test
     public void testMissingProfession() {
         assertThrows(IllegalArgumentException.class, () -> new Hero.Builder(null, "Sir without a job"));
+    }
+
+    @Test
+    public void testMissingName(){
+        assertThrows(IllegalArgumentException.class,() -> new Hero.Builder(Profession.THIEF,null));
+    }
+
+    @Test
+    public void testBuildHero(){
+        final String heroName = "Wade Anthoney";
+
+        final Hero hero = new Hero.Builder(Profession.PRIEST,heroName)
+                .armor(Armor.PLATE_MAIL)
+                .hairColor(HairColor.BLOND)
+                .hairType(HairType.CURLY)
+                .weapon(Weapon.DAGGER)
+                .build();
+
+        assertNotNull(hero);
+        assertNotNull(hero.toString());
+        assertEquals(Profession.PRIEST,hero.getProfession());
+        assertEquals(Weapon.DAGGER,hero.getWeapon());
+
+
+
     }
 }
