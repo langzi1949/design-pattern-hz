3ef54187ee2bdb944db0586e62433a8f75de3f04
chenzehao <chenzehao_1949@126.com>
2019-06-03 13:48:51 +0800

    cglib proxy method

---
+++ proxy/src/main/java/com/zmglove/cglibproxy/CglibProxyFactory.java
@@ -0,0 +1,11 @@
+package com.zmglove.cglibproxy;
+
+/**
+ * TODO
+ *
+ * @author CZH
+ * @version 1.0
+ * @date 2019/6/3 11:25
+ **/
+public class CglibProxyFactory {
+}
---
+++ proxy/src/main/java/com/zmglove/cglibproxy/UserService.java
@@ -0,0 +1,11 @@
+package com.zmglove.cglibproxy;
+
+/**
+ * TODO
+ *
+ * @author CZH
+ * @version 1.0
+ * @date 2019/6/3 11:24
+ **/
+public class UserService {
+}
