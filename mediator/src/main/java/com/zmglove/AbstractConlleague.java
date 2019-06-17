d544c50db3f426e24fc230342957efadd840b7b8
langzi <chenzehao_1949@126.com>
2019-06-17 23:32:45 +0800

    add code

--- mediator/src/main/java/com/zmglove/AbstractConlleague.java
+++ mediator/src/main/java/com/zmglove/AbstractConlleague.java
@@ -4,4 +4,10 @@ package com.zmglove;
  * 抽象同事类
  */
 public class AbstractConlleague {
+    protected  AbstractMediator mediator;
+
+
+    public AbstractConlleague(AbstractMediator mediator){
+        this.mediator = mediator;
+    }
 }
