0fbd1b4d3a04a6756567a941f3b5a40e84788ec1
langzi <chenzehao_1949@126.com>
2019-06-19 22:43:24 +0800

    mediator pattern add code

--- mediator/src/main/java/com/zmglove/AbstractMediator.java
+++ mediator/src/main/java/com/zmglove/AbstractMediator.java
@@ -1,16 +1,22 @@
 package com.zmglove;
 
 
-import lombok.AllArgsConstructor;
-
 /**
  * 抽象中介者
  */
-@AllArgsConstructor
 public abstract class AbstractMediator {
     protected Purchase purchase;
     protected Sale sale;
     protected Stock stock;
+
+    /**
+     * 构造函数
+     */
+    public AbstractMediator(){
+        this.purchase = new Purchase(this);
+        this.sale = new Sale(this);
+        this.stock = new Stock(this);
+    }
 
     /**
      * 中介者最重要的方法叫做事件方法，处理多个对象之间的关系
--- mediator/src/main/java/com/zmglove/Mediator.java
+++ mediator/src/main/java/com/zmglove/Mediator.java
@@ -1,8 +1,11 @@
 package com.zmglove;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 具体的中介者
  */
+@Slf4j
 public class Mediator extends AbstractMediator{
 
     /**
@@ -24,7 +27,49 @@ public class Mediator extends AbstractMediator{
         }
     }
 
+    /**
+     * 采购电脑
+     * @param number
+     */
     private void buyComputer(int number){
-        int saleStatus = super.sale
+        int saleStatus = super.sale.getSaleStatus();
+
+        if(saleStatus > 80){
+            log.info(">>>>采购IBM电脑:{}台",number);
+            super.stock.increase(number);
+        }else{
+            // 折半采购
+            int buyNumber = number/2;
+            log.info(">>>>采购IBM电脑:{}台",buyNumber);
+        }
+    }
+
+    /**
+     * 销售电脑
+     * @param number
+     */
+    private void sellComputer(int number){
+        // 库存不够了
+        if(super.stock.getStock() < number){
+            super.purchase.buyIBMComputer(number);
+        }
+        super.stock.decrease(number);
+    }
+
+    /**
+     * 折价销售电脑
+     */
+    private void offsell(){
+        log.info("折价销售电脑{}台",stock.getStock());
+    }
+
+    /**
+     * 清仓处理
+     */
+    private void clearStock(){
+        // 要求清仓
+        super.sale.offSale();
+        // 要求采购人员不要采购
+        super.purchase.refuseByIBM();
     }
 }
