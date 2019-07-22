d4bb811a66f1e5f3c57eea5263cd3a83ae79e55c
langzi <chenzehao_1949@126.com>
2019-07-22 22:57:50 +0800

    memento pattern code

---
+++ memento/src/main/java/com/zmglove/Originator.java
@@ -0,0 +1,19 @@
+package com.zmglove;
+
+/**
+ * 备忘录的发起人角色
+ */
+public class Originator{
+    // 内部状态
+    private String state = "";
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public 
+}
